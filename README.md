29fe121ba8490068620869b103799e84188f08e1
Dmytro Shymanskyi <dmytro.shymanskyi.dev@gmail.com>
2021-11-29 12:21:46 +0100

    added formik for form state management

--- README.md
+++ README.md
@@ -1,24 +1,42 @@
 # authModuleRN
-Created with: npx react-native init. Auth with swagger api and firebase api (with google). 
+
+Created with: npx react-native init. Auth with swagger api and firebase api (with google).
+
 ## Libraries
+
 ### Navigation:
+
 - @react-navigation/native
 - react-native-screens, react-native-safe-area-context
 - @react-navigation/native-stack
 - react-native-gesture-handler
+
 ### State management
+
 - redux
 - redux-thunk
 - react-redux
+
 ### REST
+
 - axios
+
 ### Firebase
+
 - @react-native-firebase/app
 - @react-native-google-signin/google-signin
 - @react-native-firebase/auth
+
 ### Icons
+
 - react-native-vector-icons
+
 ## Test devicies
+
 - Nexus S API 29, 480x800: hdpi, Android 10
 - Pixel XL API 29, 1440x2560, Android 10
-- Pixel C API 29(Tablet),  2560x1800, Android 10
+- Pixel C API 29(Tablet), 2560x1800, Android 10
+
+## Form management
+
+- formik
--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -7,6 +7,9 @@ import {
   Alert,
   ActivityIndicator,
 } from 'react-native';
+
+import {Formik} from 'formik';
+
 import {useDispatch, useSelector} from 'react-redux';
 import * as authActions from '../store/actions/auth';
 import {validateEmail} from '../utils/validateEmail';
@@ -14,8 +17,6 @@ import {validateEmail} from '../utils/validateEmail';
 import {styles} from './AuthFormStyles';
 
 const AuthForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [badEmail, setBadEmail] = useState(false);
   const [badPassword, setBadPassword] = useState(false);
   const loading = useSelector(state => state.loading);
@@ -25,20 +26,12 @@ const AuthForm = () => {
 
   const dispatch = useDispatch();
 
-  const submitHandler = () => {
+  const submitHandler = ({email, password}) => {
     if (!validateEmail(email)) {
       setBadEmail(true);
-      // Alert.alert(
-      //   'Неправильна поштова скринька',
-      //   'Формат поштової скриньки: example@example.com',
-      //   [{text: 'OK', onPress: () => setEmail('')}],
-      // );
       return;
     }
     if (password.trim().length < 5) {
-      // Alert.alert('Короткий пароль', 'Мінімальна довжина паролю 5 знаків', [
-      //   {text: 'OK', onPress: () => setPassword('')},
-      // ]);
       setBadPassword(true);
       return;
     }
@@ -52,41 +45,49 @@ const AuthForm = () => {
   }
 
   return (
-    <View style={styles.formContainer}>
-      <TextInput
-        style={[styles.input, badEmail ? styles.inpurError : null]}
-        value={email}
-        placeholder="Поштова скринька"
-        onChangeText={text => setEmail(text)}
-        keyboardType="email-address"
-        caretHidden={false}
-        returnKeyType="next"
-        blurOnSubmit={false}
-        onFocus={() => setBadEmail(false)}
-        onSubmitEditing={() => {
-          secondInputRef.current.focus();
-        }}
-      />
-      <TextInput
-        style={[styles.input, badPassword ? styles.inpurError : null]}
-        value={password}
-        placeholder="Пароль"
-        onChangeText={text => setPassword(text)}
-        onFocus={() => setBadPassword(false)}
-        caretHidden={false}
-        ref={secondInputRef}
-      />
-      <TouchableOpacity style={styles.forgotPasswordContainer}>
-        <Text style={styles.forgotPasswordText}>Забули пароль?</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.submitButton} onPress={submitHandler}>
-        {loading ? (
-          <ActivityIndicator size="small" color="#fff" />
-        ) : (
-          <Text style={styles.submitText}>Увійти</Text>
-        )}
-      </TouchableOpacity>
-    </View>
+    <Formik
+      initialValues={{email: '', password: ''}}
+      onSubmit={values => {
+        submitHandler(values);
+      }}>
+      {({handleChange, handleSubmit, values}) => (
+        <View style={styles.formContainer}>
+          <TextInput
+            style={[styles.input, badEmail ? styles.inpurError : null]}
+            value={values.email}
+            placeholder="Поштова скринька"
+            onChangeText={handleChange('email')}
+            keyboardType="email-address"
+            caretHidden={false}
+            returnKeyType="next"
+            blurOnSubmit={false}
+            onFocus={() => setBadEmail(false)}
+            onSubmitEditing={() => {
+              secondInputRef.current.focus();
+            }}
+          />
+          <TextInput
+            style={[styles.input, badPassword ? styles.inpurError : null]}
+            value={values.password}
+            placeholder="Пароль"
+            onChangeText={handleChange('password')}
+            onFocus={() => setBadPassword(false)}
+            caretHidden={false}
+            ref={secondInputRef}
+          />
+          <TouchableOpacity style={styles.forgotPasswordContainer}>
+            <Text style={styles.forgotPasswordText}>Забули пароль?</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
+            {loading ? (
+              <ActivityIndicator size="small" color="#fff" />
+            ) : (
+              <Text style={styles.submitText}>Увійти</Text>
+            )}
+          </TouchableOpacity>
+        </View>
+      )}
+    </Formik>
   );
 };
 
